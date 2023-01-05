01507f979b22c38c0fa5f230246f3ce75af8f17e
Gokhan Sigircik <gokhansigircik@hotmail.com>
2023-01-05 11:30:38 -0500

    adding some algo

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,8 @@
+// What is FizzBuzz?
+// Firstly, let’s get this out of the way, FizzBuzz is a task where the programmer is asked 
+// to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and 
+// similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
+
 
 for (var i = 1; i < 101; i++) {
     if (i % 15 == 0) {
--- loopchal.js
+++ loopchal.js
@@ -47,3 +47,16 @@ for(var i=1; i<13; i++) {
 }
 console.log(product);
 
+// i need to do more algos and get better for the interviews
+
+// What is FizzBuzz?
+// Firstly, let’s get this out of the way, FizzBuzz is a task where the programmer is asked 
+// to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and 
+// similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
+
+for (var i = 1; i < 101; i++) {
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
+}
